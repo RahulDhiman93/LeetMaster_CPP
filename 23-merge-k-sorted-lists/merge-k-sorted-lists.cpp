2d3ed2cc119a75a9ca07c33c11d36977d92f4bbd
Rahul Dhiman <rsdm34@gmail.com>
2024-07-11 11:12:16 -0700

    Time: 12 ms (84.46%) | Memory: 18.1 MB (7.54%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,22 +11,58 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> sa;
-        for(int i=0;i<lists.size();++i) {
-            ListNode* node = lists[i];
-            while(node != NULL) {
-                sa.push_back(node->val);
-                node = node->next;
-            }
-        }
-        sort(sa.begin(), sa.end());
+        int n = lists.size();
+        if (n == 0) return NULL;
         
-        ListNode* head = new ListNode();
-        ListNode* node = head;
-        for(int i=0;i<sa.size();++i) {
-            node->next = new ListNode(sa[i]);
-            node = node->next;
+        //MERGE SORT
+        while (n > 1) {
+            for (int i = 0; i < n / 2; i++) {
+                lists[i] = merge2Lists(lists[i], lists[n - i - 1]);
+            }
+            n = (n + 1) / 2;
         }
-        return head->next;
+        
+        return lists[0];
+    }
+private:
+    ListNode* merge2Lists(ListNode* list1, ListNode* list2) {
+        if (list1 == NULL && list2 == NULL) {
+            return NULL;
+        }
+        if (list1 == NULL) {
+            return list2;
+        }
+        if (list2 == NULL) {
+            return list1;
+        }
+        
+        ListNode* head = NULL;
+        if (list1->val <= list2->val) {
+            head = list1;
+            list1 = list1->next;
+        } else {
+            head = list2;
+            list2 = list2->next;
+        }
+        ListNode* curr = head;
+        
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        
+        if (list1 == NULL) {
+            curr->next = list2;
+        } else {
+            curr->next = list1;
+        }
+        
+        return head;
     }
 };
