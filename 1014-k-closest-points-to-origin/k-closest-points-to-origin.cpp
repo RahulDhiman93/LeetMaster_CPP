6c1e38f5fc22c1a42bc884aa7ebdd56d00b44bc8
Rahul Dhiman <rsdm34@gmail.com>
2024-07-22 16:45:15 -0700

    Time: 233 ms (13.71%) | Memory: 104 MB (6.98%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<double, vector<double>, greater<double>> pq;
+        unordered_map<double, vector<vector<int>>> umap;
+        for (vector<int> point : points) {
+            double dist = sqrt(point[0] * point[0] + point[1] * point[1]);
+            umap[dist].push_back(point);
+            pq.push(dist);
+        }
+        vector<vector<int>> res;
+        while(k) {
+            for(vector<int> point: umap[pq.top()]) {
+                res.push_back(point);
+                pq.pop();
+                k--;
+            }
+        }
+        return res;
+    }
+};
