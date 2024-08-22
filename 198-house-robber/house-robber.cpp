defad7a9741faa5a2c27aae6c866728d8830117b
Rahul Dhiman <rsdm34@gmail.com>
2024-08-22 01:26:48 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (45.07%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        } else if (n == 2) {
+            return max(nums[0], nums[1]);
+        }
+        
+        for (int i = n - 3; i >= 0; --i) {
+            if (i == n - 3) {
+                nums[i] += nums[i+2];
+            } else {
+                nums[i] += max(nums[i+2], nums[i+3]);
+            }
+        }
+        return max(nums[0], nums[1]);
+    }
+};
