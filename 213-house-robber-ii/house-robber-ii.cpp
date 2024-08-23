db26de2dcd75bf158df15645ff5094dabf8e533d
Rahul Dhiman <rsdm34@gmail.com>
2024-08-23 01:52:19 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (55.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        return max(nums[0], max(helper(nums, 0, n-2), helper(nums, 1, n-1)));
+        if (n==1)
+            return nums[0];
+        return max(helper(nums, 0, n-2), helper(nums, 1, n-1));
     }
 private:
     int helper(vector<int>& nums, int s, int n) {
