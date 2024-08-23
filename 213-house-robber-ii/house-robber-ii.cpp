6ebe95d1e5319d3bb3288b9fe22c350a7d3f9e66
Rahul Dhiman <rsdm34@gmail.com>
2024-08-23 01:51:27 -0700

    Time: 4 ms (32.76%) | Memory: 9.5 MB (55.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,34 +2,20 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return nums[0];
-        } else if (n == 2) {
-            return max(nums[0], nums[1]);
-        } else if (n == 3) {
-            return max(nums[0], max(nums[1], nums[2]));
-        }
-        int res1 = helper(nums, n, true);
-        int res2 = helper(nums, n, false);
-        return max(res1, res2);
+        return max(nums[0], max(helper(nums, 0, n-2), helper(nums, 1, n-1)));
     }
-
 private:
-    int helper(vector<int> nums, int n, bool includeLast) {
-        if (!includeLast)
-            nums[n-1] = 0;
-        for (int i = n - 3; i >= 0; --i) {
-            if (i == n - 3) {
-                nums[i] += nums[i+2];
-            } else {
-                nums[i] += max(nums[i+2], nums[i+3]);
-            }
+    int helper(vector<int>& nums, int s, int n) {
+        int prev = 0;
+        int curr = 0;
+        int next = 0;
+        
+        for (int i = s; i <= n; i++) {
+            next = max(prev + nums[i], curr);
+            prev = curr;
+            curr = next;
         }
-        if (includeLast)
-            return max(nums[1], nums[2]);
-        else
-            return nums[0];
+        
+        return curr;
     }
 };
