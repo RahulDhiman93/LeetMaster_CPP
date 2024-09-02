8e888708dc41ef8ba9257764efadc60163bdf141
Rahul Dhiman <rsdm34@gmail.com>
2024-09-02 15:26:51 -0700

    Time: 4 ms (12.41%) | Memory: 7.5 MB (87.51%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int> (n, -1));
-        return helper(memo, 0, 0, m, n);
-    }
+    int uniquePaths(int m, int n) { 
+        vector<int> row(n, 1); 
 
-private:
-    int helper(vector<vector<int>>& memo, int i, int j, int m, int n) {
-        if (i >= m || j >= n)
-            return 0;
-        if (i == m - 1 || j == n - 1)
-            return 1;
-        if (memo[i][j] != -1)
-            return memo[i][j];
-        
-        memo[i][j] = helper(memo, i + 1, j, m, n) + helper(memo, i, j + 1, m, n);
-        return memo[i][j];
+        for(int i = 0; i < m - 1; ++i) {
+            vector<int> newRow(n, 1);
+            for(int j = n - 2; j >= 0 ; --j) {
+                newRow[j] = newRow[j + 1] + row[j];
+            }
+            row = newRow;
+        }
+
+        return row[0];
     }
 };
