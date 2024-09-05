a44db3a1603faa7a8d020bcd27ec501714fb3b20
Rahul Dhiman <rsdm34@gmail.com>
2024-09-05 01:37:29 -0700

    Time: 9 ms (18.16%) | Memory: 14.7 MB (31.29%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        map<pair<int, bool>, int> dp;
+        vector<vector<int>> dp(prices.size()+2,vector<int>(2,-1));
         return dfs(dp, prices, 0, true);
     }
 
 private:
-    int dfs(map<pair<int, bool>, int>& dp, vector<int>& prices, int i, bool isBuying) {
+    int dfs(vector<vector<int>>& dp, vector<int>& prices, int i, bool isBuying) {
         if (i >= prices.size())
             return 0;
-        if (dp.contains({i, isBuying}))
-            return dp[{i, isBuying}];
+        if (dp[i][isBuying] != -1)
+            return dp[i][isBuying];
 
         if (isBuying) {
             int buy = dfs(dp, prices, i + 1, !isBuying) - prices[i];
             int cooldown = dfs(dp, prices, i + 1, isBuying);
-            dp[{i, isBuying}] = max(buy, cooldown);
+            dp[i][isBuying] = max(buy, cooldown);
         } else {
             int sell = dfs(dp, prices, i + 2, !isBuying) + prices[i];
             int cooldown = dfs(dp, prices, i + 1, isBuying);
-            dp[{i, isBuying}] = max(sell, cooldown);
+            dp[i][isBuying] = max(sell, cooldown);
         }
-        return dp[{i, isBuying}];
+        return dp[i][isBuying];
     }
 };
