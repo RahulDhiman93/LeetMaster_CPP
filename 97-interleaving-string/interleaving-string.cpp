fa801b3fa734ab18cdf411abc5c4bc7db7e1fd7a
Rahul Dhiman <rsdm34@gmail.com>
2024-09-12 00:17:24 -0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (67.13%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -5,22 +5,41 @@ public:
         int n = s2.size();
         if (m + n != s3.size())
             return false;
-        map<pair<int, int>, bool> dp;
-        return dfs(dp, s1, s2, s3, 0, 0, m, n);
+
+        return tdp(s1, s2, s3, 0, 0, m, n);
+        // map<pair<int, int>, bool> dp;
+        // return dfs(dp, s1, s2, s3, 0, 0, m, n);
     }
 
 private:
-    bool dfs(map<pair<int, int>, bool>& dp, string& s1, string& s2, string& s3, int i, int j, int m, int n) {
+    bool tdp(string& s1, string& s2, string& s3, int i, int j, int m, int n) {
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[m][n] = true;
+
+        for (int i = m; i >= 0; --i) {
+            for (int j = n; j >= 0; --j) {
+                if (i < m && s1[i] == s3[i + j] && dp[i + 1][j])
+                    dp[i][j] = true;
+                if (j < n && s2[j] == s3[i + j] && dp[i][j + 1])
+                    dp[i][j] = true;
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    bool dfs(map<pair<int, int>, bool>& dp, string& s1, string& s2, string& s3,
+             int i, int j, int m, int n) {
         if (i == m && j == n)
             return true;
         if (dp.contains({i, j}))
             return dp[{i, j}];
-        
-        if (i < m && s1[i] == s3[i+j] && dfs(dp, s1, s2, s3, i + 1, j, m, n))
+
+        if (i < m && s1[i] == s3[i + j] && dfs(dp, s1, s2, s3, i + 1, j, m, n))
             return true;
-        if (j < n && s2[j] == s3[i+j] && dfs(dp, s1, s2, s3, i, j + 1, m, n))
+        if (j < n && s2[j] == s3[i + j] && dfs(dp, s1, s2, s3, i, j + 1, m, n))
             return true;
-            
+
         dp[{i, j}] = false;
         return false;
     }
