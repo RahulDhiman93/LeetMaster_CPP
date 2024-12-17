0f35715247d61e16f8a53f6b7a9602c8316c8628
Rahul Dhiman <rsdm34@gmail.com>
2024-12-17 02:50:09 -0800

    Time: 0 ms (100.00%) | Memory: 52.3 MB (28.32%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int res = nums.size() - 1;
+
+        for(int i = res; i >= 0; --i) {
+            if (i + nums[i] >= res)
+                res = i;
+        }
+
+        return res == 0;
+    }
+};
