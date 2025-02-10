aea34b60ea5065dc89bce814d403c2e14656cf31
Rahul Dhiman <rsdm34@gmail.com>
2025-02-09 19:52:21 -0800

    Time: 0 ms (100.00%) | Memory: 22 MB (46.48%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -12,13 +12,14 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return valid(root, LONG_MIN, LONG_MAX);
+        return helper(root, LONG_MIN, LONG_MAX);
     }
+
 private:
-    bool valid(TreeNode* root, long left, long right) {
+    bool helper(TreeNode* root, long left, long right) {
         if (root == NULL) return true;
-        if (root->val < right && root->val > left) {
-            return valid(root->left, left, root->val) && valid(root->right, root->val, right);
+        if (root->val > left && root->val < right) {
+            return (helper(root->left, left, root->val) && helper(root->right, root->val, right));
         }
         return false;
     }
