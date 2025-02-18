ff9ede08e075a816dff82fd075fe429a3cffc587
Rahul Dhiman <rsdm34@gmail.com>
2025-02-18 00:34:27 -0800

    Time: 27 ms (19.24%) | Memory: 29.1 MB (16.26%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -3,40 +3,42 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        vector<vector<bool>> visP(m, vector<bool>(n, false));
-        vector<vector<bool>> visA(m, vector<bool>(n, false));
-
-        for (int i = 0; i < m; ++i) {
-            dfs(i, 0, m, n, visP, heights[i][0], heights);
-            dfs(i, n - 1, m, n, visA, heights[i][n - 1], heights);
-        }
-
-        for (int j = 0; j < n; ++j) {
-            dfs(0, j, m, n, visP, heights[0][j], heights);
-            dfs(m - 1, j, m, n, visA, heights[m - 1][j], heights);
-        }
-
         vector<vector<int>> res;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (visP[i][j] && visA[i][j]) {
+
+        set<pair<int, int>> pacificVisited;
+        set<pair<int, int>> atlanticVisited;
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacificVisited, i, 0, m, n, 0);
+            dfs(heights, atlanticVisited, i, n - 1, m, n, 0);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacificVisited, 0, j, m, n, 0);
+            dfs(heights, atlanticVisited, m - 1, j, m, n, 0);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacificVisited.contains({i, j}) && atlanticVisited.contains({i, j})) {
                     res.push_back({i, j});
                 }
             }
         }
+
         return res;
     }
 
 private:
-    void dfs(int r, int c, int m, int n, vector<vector<bool>>& visited,
-             int prevH, vector<vector<int>>& heights) {
-        if (r < 0 || c < 0 || r >= m || c >= n ||
-            prevH > heights[r][c] || visited[r][c])
+    void dfs(vector<vector<int>>& heights, set<pair<int, int>>& visited, int i, int j, int m, int n, int prev) {
+        if (i < 0 || j < 0 || i >= m || j >= n || heights[i][j] < prev || visited.contains({i, j}))
             return;
-        visited[r][c] = true;
-        dfs(r+1, c, m, n, visited, heights[r][c], heights);
-        dfs(r-1, c, m, n, visited, heights[r][c], heights);
-        dfs(r, c+1, m, n, visited, heights[r][c], heights);
-        dfs(r, c-1, m, n, visited, heights[r][c], heights);
+
+        visited.insert({i, j});
+
+        dfs(heights, visited, i + 1, j, m, n, heights[i][j]);
+        dfs(heights, visited, i - 1, j, m, n, heights[i][j]);
+        dfs(heights, visited, i, j + 1, m, n, heights[i][j]);
+        dfs(heights, visited, i, j - 1, m, n, heights[i][j]);
     }
 };
