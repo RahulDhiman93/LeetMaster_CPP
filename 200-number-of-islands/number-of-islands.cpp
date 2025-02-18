72fd7871963a51906ef3b4d0fdcfceaf30a9e451
Rahul Dhiman <rsdm34@gmail.com>
2025-02-18 01:07:24 -0800

    Time: 25 ms (72.88%) | Memory: 16.2 MB (89.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,14 +3,12 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        
-        set<pair<int, int>> visited;
         int res = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited.contains({i, j})) {
-                    dfs(grid, visited, i, j, m, n);
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j, m, n);
                     res++;
                 }
             }
@@ -20,15 +18,15 @@ public:
     }
 
 private:
-    void dfs(vector<vector<char>>& grid, set<pair<int, int>>& visited, int i, int j, int& m, int& n) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0' || visited.contains({i, j}))
+    void dfs(vector<vector<char>>& grid, int i, int j, int& m, int& n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
             return;
         
-        visited.insert({i, j});
+        grid[i][j] = '0';
         
-        dfs(grid, visited, i + 1, j, m, n);
-        dfs(grid, visited, i - 1, j, m, n);
-        dfs(grid, visited, i, j + 1, m, n);
-        dfs(grid, visited, i, j - 1, m, n);
+        dfs(grid, i + 1, j, m, n);
+        dfs(grid, i - 1, j, m, n);
+        dfs(grid, i, j + 1, m, n);
+        dfs(grid, i, j - 1, m, n);
     }
 };
