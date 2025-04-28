44e4793b890ac78f5885648e04b3997703a93a12
Rahul Dhiman <rsdm34@gmail.com>
2025-04-27 21:55:32 -0700

    Time: 0 ms (100.00%) | Memory: 10.3 MB (28.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str = getSimpleStr(s);
-        for(int i=0,j=str.size()-1; i<str.size() && j>=0; ++i, --j) {
-            if (str[i] != str[j]) {
-                return false;
-            }
-        }
-        return true;
-    }
+        vector<char> str;
+        int diff = 'A' - 'a';
 
-private:
-    string getSimpleStr(string str) {
-        string ss;
-        for(char c : str) {
-            if (c>='A' && c <='Z') {
-                ss.push_back(static_cast<char>(tolower(c)));
-            } else if (c>='a' && c<='z') {
-                ss.push_back(c);
-            } else if (c>='0' && c<='9') {
-                ss.push_back(c);
+        for (char c : s) {
+            if (c >= 'A' && c <= 'Z') {
+                str.push_back(c - diff);
+            } else if (c >= 'a' && c <= 'z') {
+                str.push_back(c);
+            } else if (c >= '0' && c <= '9') {
+                str.push_back(c);
             }
         }
-        return ss;
+
+        int i = 0, j = str.size() - 1;
+
+        while(i < j) {
+            if(str[i] != str[j])
+                return false;
+            
+            i++; j--;
+        }
+
+        return true;
     }
 };
