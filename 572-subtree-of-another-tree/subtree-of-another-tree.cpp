3fb6fad083456159fddb9b0db2b7c85be8b03d4f
Rahul Dhiman <rsdm34@gmail.com>
2025-05-05 19:12:15 -0700

    Time: 0 ms (100.00%) | Memory: 29 MB (65.97%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -12,16 +12,33 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL) return false;
-        if (isSameTree(root, subRoot)) return true;
-        return (isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot));
+        if (root == nullptr && subRoot == nullptr) {
+            return true;
+        } else if (root != nullptr && subRoot == nullptr) {
+            return true;
+        } else if (root == nullptr && subRoot != nullptr) {
+            return false;
+        }
+        
+        if (root->val == subRoot->val && (isSameTree(root, subRoot))) {
+            return true;
+        } else {
+            return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        }
     }
 
 private:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
-        if ((p == NULL && q != NULL) || (p != NULL && q == NULL)) return false;
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p != nullptr && q == nullptr) {
+            return false;
+        } else if (p == nullptr && q != nullptr) {
+            return false;
+        }
+
         if (p->val != q->val) return false;
+
         return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
