8e47574e450cb10b915ef0f87c2ff4469933d1ec
Rahul Dhiman <rsdm34@gmail.com>
2025-05-08 22:44:26 -0700

    Time: 191 ms (89.36%) | Memory: 10.3 MB (99.15%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -3,27 +3,33 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-        for(int i=0;i<m;++i) {
-            for(int j=0;j<n;++j) {
-                if(board[i][j] == word[0]) {
-                    if(helper(board, word, 0, i, j, m, n)) return true;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, 0, i, j, m, n)) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
+
 private:
-    bool helper(vector<vector<char>>& board, string word, int index, int i, int j, int m, int n) {
-        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word[index]) return false;
-        if(index == word.size()-1) return true;
+    bool dfs(vector<vector<char>>& board, string& word, int index, int i, int j, int m, int n) {
+        if (index == word.size())
+            return true;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || index >= word.size() || board[i][j] != word[index])
+            return false;
+
+        char temp = board[i][j];
         board[i][j] = '*';
-        if(helper(board, word, index+1, i, j+1, m, n) ||
-           helper(board, word, index+1, i, j-1, m, n) ||
-           helper(board, word, index+1, i+1, j, m, n) ||
-           helper(board, word, index+1, i-1, j, m, n)) {
-                return true;
-        }
-        board[i][j] = word[index];
-        return false;
+        bool res = dfs(board, word, index + 1, i + 1, j, m, n) ||
+                   dfs(board, word, index + 1, i - 1, j, m, n) ||
+                   dfs(board, word, index + 1, i, j + 1, m, n) ||
+                   dfs(board, word, index + 1, i, j - 1, m, n);
+        board[i][j] = temp;
+        return res;
     }
 };
