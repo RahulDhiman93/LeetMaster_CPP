eb740423993a886aa1d5eb5af0dd28f0d71e1bc2
Rahul Dhiman <rsdm34@gmail.com>
2025-05-13 18:03:43 -0700

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,24 +2,12 @@ class Solution {
 public:
     int climbStairs(int n) {
         if (n <= 2) return n;
-        unordered_map<int, int> memo;
-        int res = dp(memo, 1, n) + dp(memo, 2, n);
-        return res;
-    }
-
-private:
-    int dp(unordered_map<int, int>& memo, int curr, int n) {
-        if (curr == n)
-            return 1;
-        
-        if (curr > n)
-            return 0;
-        
-        if (memo.contains(curr)) {
-            return memo[curr];
+        int one = 1, two = 1;
+        for (int i = n-2; i >=0; i--) {
+            int temp = one;
+            one = one + two;
+            two = temp;
         }
-        
-        memo[curr] = dp(memo, curr+1, n) + dp(memo, curr+2, n);
-        return memo[curr];
+        return one;
     }
 };
