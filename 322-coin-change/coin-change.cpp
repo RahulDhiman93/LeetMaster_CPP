faa76e7f93d2d72a880e2d92552cfbbb2fa455a6
Rahul Dhiman <rsdm34@gmail.com>
2025-05-18 02:45:54 -0700

    Time: 15 ms (92.19%) | Memory: 17.9 MB (81.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,7 +3,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1, amount+1);
         dp[0] = 0;
-        for(int a = 1; a < amount + 1; ++a) {
+        for(int a = 1; a < amount +1; ++a) {
             for(int c: coins) {
                 if (a - c >= 0) {
                     dp[a] = min(dp[a], 1 + dp[a - c]);
