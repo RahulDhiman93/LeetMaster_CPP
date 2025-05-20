564328195df93fdd1ff1ade3650b45f2c6113dd2
Rahul Dhiman <rsdm34@gmail.com>
2025-05-19 21:16:28 -0700

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.36%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n + 1, 1);
-        unordered_set<char> numS = {'0', '1', '2', '3', '4', '5', '6'};
-        for (int i = n - 1; i >= 0; i--) {
+        vector<int> dp(s.size() + 1);
+        dp[s.size()] = 1;
+
+        for(int i = s.size() - 1; i >= 0; --i) {
             if (s[i] == '0') {
                 dp[i] = 0;
             } else {
-                dp[i] = dp[i + 1];
-            }
-            if (i + 1 < n &&
-                (s[i] == '1' || (s[i] == '2' && numS.contains(s[i + 1])))) {
-                dp[i] += dp[i + 2];
+                dp[i] = dp[i+1];
+                if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) {
+                    dp[i] += dp[i+2];
+                }
             }
         }
+
         return dp[0];
     }
 };
