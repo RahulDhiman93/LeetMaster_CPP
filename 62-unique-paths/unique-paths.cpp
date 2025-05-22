0285cab140874ed05117e285bd10fb0f90d4b3d5
Rahul Dhiman <rsdm34@gmail.com>
2025-05-22 01:25:04 -0700

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.01%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> row(n, 1);
+        vector<vector<int>> memo(m, vector<int> (n, 0));
+        memo[m-1][n-1] = 1;
+        return dfs(memo, 0, 0, m, n);
+    }
 
-        for (int i = 0; i < m - 1; ++i) {
-            vector<int> newRow(n, 1);
-            for (int j = n - 2; j >= 0; --j) {
-                newRow[j] = newRow[j + 1] + row[j];
-            }
-            row = newRow;
+private:
+    int dfs(vector<vector<int>>& memo, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return 0;
         }
 
-        return row[0];
+        if (memo[i][j] != 0) {
+            return memo[i][j];
+        }
+
+        memo[i][j] = dfs(memo, i+1, j, m, n) + dfs(memo, i, j+1, m, n);
+        return memo[i][j];
     }
 };
