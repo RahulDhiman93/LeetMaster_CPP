a298f6626910cb78bc92490f9c2373f45a56176a
Rahul Dhiman <rsdm34@gmail.com>
2025-07-30 01:02:34 -0700

    Time: 4 ms (16.71%) | Memory: 16.8 MB (75.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,56 +3,62 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int res = 0, ora = 0;
 
         queue<pair<int, int>> q;
-
-        for(int i=0;i<m;++i) {
-            for(int j=0;j<n;++j) {
-                if(grid[i][j]==2) {
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
                     q.push({i, j});
-                } else if(grid[i][j]==1) {
-                    ora++;
                 }
             }
         }
 
-        while(!q.empty() && ora > 0) {
-            int size = q.size();
-            while(size) {
-                int i = q.front().first;
-                int j = q.front().second;
-                q.pop();
-                
-                if(i>0 && grid[i-1][j] == 1) {
-                    grid[i-1][j] = 2;
-                    q.push({i-1, j});
-                    ora--;
-                }
+        int res = 0;
+        while(!q.empty()) {
+            int count = q.size();
+            bool isRot = false;
 
-                if(i<m-1 && grid[i+1][j] == 1) {
+            while(count--) {
+                pair<int, int> top = q.front(); q.pop();
+                int i = top.first, j = top.second;
+
+                if (i + 1 < m && grid[i+1][j] == 1) {
                     grid[i+1][j] = 2;
                     q.push({i+1, j});
-                    ora--;
+                    isRot = true;
                 }
-
-                if(j>0 && grid[i][j-1] == 1) {
-                    grid[i][j-1] = 2;
-                    q.push({i, j-1});
-                    ora--;
-                }
-
-                if(j<n-1 && grid[i][j+1] == 1) {
+                if (j + 1 < n && grid[i][j+1] == 1) {
                     grid[i][j+1] = 2;
                     q.push({i, j+1});
-                    ora--;
+                    isRot = true;
                 }
-
-                size--;
+                if (i - 1 >= 0 && grid[i-1][j] == 1) {
+                    grid[i-1][j] = 2;
+                    q.push({i-1, j});
+                    isRot = true;
+                }
+                if (j - 1 >= 0 && grid[i][j-1] == 1) {
+                    grid[i][j-1] = 2;
+                    q.push({i, j-1});
+                    isRot = true;
+                }
             }
-            res++;
+
+            if (isRot) {
+                res++;
+            }
         }
 
-        return ora == 0 ? res : -1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res = -1;
+                    break;
+                }
+            }
+        }
+
+        return res;
     }
 };
