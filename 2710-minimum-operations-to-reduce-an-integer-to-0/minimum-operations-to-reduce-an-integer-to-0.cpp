dd5b32ed5f273d174fdea3b774cf0995becf0fc3
Rahul Dhiman <rsdm34@gmail.com>
2025-08-03 18:32:09 -0700

    Time: 0 ms (100.00%) | Memory: 8 MB (37.39%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,26 +1,14 @@
 class Solution {
-private:
-    unordered_map<int, int> dp;
-
-    int dfs(int n) {
-        if (n == 0)
-            return 0;
-        if (dp.contains(n))
-            return dp[n];
-
-        if ((n & (n - 1)) == 0)
-            return dp[n] == 1;
-
-        int p = 1;
-        while (p < n)
-            p <<= 1;
-
-        int add = 1 + dfs(p - n);
-        int sub = 1 + dfs(n - (p >> 1));
-
-        return dp[n] = min(add, sub);
-    }
-
 public:
-    int minOperations(int n) { return dfs(n) + 1; }
+    int minOperations(int n) {
+        int count = 0;
+        while (n != 0) {
+            int x = floor(log2(n));
+            int y = ceil(log2(n));
+            int ans1 = pow(2, x), ans2 = pow(2, y);
+            n = (min(abs(n - ans1), abs(n - ans2)));
+            count++;
+        }
+        return count;
+    }
 };
