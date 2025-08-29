29c2b773b4041f1af0b9df6af33579725c7e6bb7
Rahul Dhiman <rsdm34@gmail.com>
2025-08-29 14:27:43 -0700

    Time: 12 ms (53.26%) | Memory: 17.9 MB (64.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+            
+        hashMap = {}
+
+        for c in s:
+            if c not in hashMap:
+                hashMap[c] = 1
+            else:
+                hashMap[c] += 1
+        
+        for c in t:
+            if c in hashMap:
+                hashMap[c] -= 1
+        
+        print(hashMap)
+        for key, value in hashMap.items():
+            if value != 0:
+                return False
+        
+        return True
+        
